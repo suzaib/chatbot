78011a27291bfeb425ea4e802619542180ad7d44
suzaib <suzaib.work@gmail.com>
2026-06-03 19:24:23 +0530

    new

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const cookieParser=require('cookie-parser');
 const cors=require('cors');
 const dotenv=require('dotenv');
 const connectDB=require('./config/dbConnect')
+const bodyParser=require('body-parser');
+const authRoute=require('./routes/authRoute');
 
 dotenv.config();
 
@@ -13,10 +15,14 @@ const app=express();
 //Middlewares
 app.use(express.json()); //Parse body data
 app.use(cookieParser()); //Parse token on every request
+app.use(bodyParse.urlencoded({extension:true}));
 
 //Database Connection
 connectDB();
 
+
+//Routes
+app.use('/api/auth',authRoute);
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
 })
