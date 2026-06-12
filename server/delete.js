658a4f84f0f22074152f1d02a2658bdb15eeca01
Suzaib <suzaib.work@gmail.com>
2026-06-12 19:56:53 +0530

    new

--- server/delete.js
+++ server/delete.js
@@ -1,11 +1,30 @@
-let p=new Promise((resolve,reject)=>{
-    let a=1+1;
-    if(a==2) resolve("Success");
-    else reject("failed");
-})
+const multer=require('multer');
 
-p.then((message)=>{
-    console.log(message);
-}).catch((err)=>{
-    console.log(message);
-})
+const cloudinary=require('cloudinary');
+const dotenv=require('dotenv');
+const fs=require('fs');
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadFileToCloudinary=(file)=>{
+    const options={
+        resource_type:file.mimetype.startWith('video')? 'video':'image'
+    }
+
+    return new Promise((resolve,reject)=>{
+        const uploader=file.mimetype.startWith('video')? cloudinary.uploader.upload_large:cloudinary.uploader.upload
+        uploader(file.path,options,(error,result)=>{
+            fs.unlink(file,path,()=>{
+                fs.unlink(file.path,()=>{
+                    if(error) reject(error);
+                    resolve (result);
+                })
+            })
+        })
+    })
+}
