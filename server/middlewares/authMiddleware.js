529822d8f2da6a90002b0e0a3dda57111e57de68
Suzaib <suzaib.work@gmail.com>
2026-06-17 02:01:23 +0530

    New

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -2,4 +2,16 @@ const jwt=require('jsonwebtoken');
 
 const authMiddleware=(req,res,next)=>{
     const authToken=req.cookies.auth_token;
+
+    if(!auth_token) return response(res,401,'Authorization token missing\nPlease provide token');
+    try {
+        const decode=jwt.verify(authToken,process.env.JWT_SECRET);
+        req.user=decode;
+        next();
+    } catch (error) {
+        console.error(error);
+        return response(res,401,'Invalid or expired token');
+    }
 }
+
+module.exports=authMiddleware;
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,9 +1,14 @@
 const express=require('express');
 
 const authController=require('../controllers/authController');
+const authMiddleware = require('../middlewares/authMiddleware');
+const { multerMiddleware } = require('../config/cloudinaryConfig');
 const router=express.Router();
 
 router.post('/send-otp',authController.sendOTP);
 router.pose('/verify-otp',authController.verifyOTP);
+
+//Protected Route
+router.put('/update-profile',authMiddleware,multerMiddleware,authController.updateProfile)
 
 module.exports=router;
