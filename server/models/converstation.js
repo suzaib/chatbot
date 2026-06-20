3f8d7c90bd9ae04a25a861e44d290ed292a73c65
Suzaib <suzaib.work@gmail.com>
2026-06-21 03:38:39 +0530

    NEW

--- server/models/converstation.js
+++ server/models/converstation.js
@@ -5,6 +5,8 @@ module.exports=Conversation
 
 
 const conversationSchema=new mongoose.Schema({
+
+    //We store an array of objects since we need id of participants which are more than 1
     participants:[{
         type:mongoose.Schema.Types.ObjectId,
         ref:"User"
