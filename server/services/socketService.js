aef1e8acb34e63315387b963a30c284c6533e89e
Suzaib <suzaib.work@gmail.com>
2026-07-15 03:52:44 +0530

    new

--- server/services/socketService.js
+++ server/services/socketService.js
@@ -4,6 +4,7 @@ const Message=require("../models/message")
 
 
 //We need all the users who are online
+//It has the mapping : userId --> socketId
 const onlineUsers=new Map();
 
 //We need a list of users who are currently typing
@@ -86,7 +87,151 @@ const initializeSocket=(server)=>{
                     })
                 }
             }
-            catch(error) console.error("Error updating message read status",error); 
+            catch(error){
+                console.error("Error updating message read status",error); 
+            }
         })
-    })
+
+        //Handle typing start event and stop after 3s
+        socket.on("typing_start",({conversationId,receiverId})=>{
+            if(!userId || !conversationId || !receiverId) return;
+
+            if(!typingUsers.has(userId)) typingUsers.set(userId,{});
+
+            const userTyping=typingUsers.get(userId);
+            userTyping[conversationId]=true;
+
+            //Clear any exiting timeout
+            if(userTyping[`${conversationId}_timeout`]) clearTimeout(userTyping[`${conversationId}_timeout`]);
+
+            //Create new interval that stops the timeout after 3s
+            userTyping[`${conversationId}_timeout`]=setTimeout(()=>{
+                userTyping[conversationId]=false;
+                socket.to(receiverId).emit("user_typing",{
+                    userId,
+                    conversationId,
+                    isTyping:false,
+                })
+            },3000)
+
+            //Notify the receiver
+            socket.to(receiverId).emit("user_typing",{
+                userId,
+                conversationId,
+                isTyping:true
+            })
+        })
+
+
+        socket.on("typing_stop",({conversationId,receiverId})=>{
+            if(!userId || !conversationId || !receiverId) return;
+
+            if(typingUsers.has(userId)){
+                const userTyping=typingUsers.get(userId);
+                userTyping[conversationId]=false;
+
+                if(userTyping[`${conversationId}_timeout`]){
+                    clearTimeout(userTyping[`${conversationId}_timeout`]);
+                    delete userTyping[`${conversationId}_timeout`];
+                }
+            }
+
+            socket.to(receiverId).emit("user_typing",{
+                userId,
+                conversationId,
+                isTyping:false
+            })
+        })
+
+        //Add or update reactions on a message
+        socket.on("add_reactions",async({messageId,emoji,userId,reactionUserId})=>{
+            try{
+                const message=await Message.findById(messageId);
+                if(!message) return;
+
+                const existingReactionIdx=message.reactions.findIndex((r)=>{
+                    r.user.toString()===reactionUserId;
+                })
+
+                //If the message already has a reaction(emoji)
+                if(existingReactionIdx==-1){
+                    const existingReaction=message.reaction.find(existingReactionIdx);
+
+                    //If the emoji is the same as the one we are putting, then toggle remove it
+                    if(existing.emoji===emoji) message.reactions.splice(existingReactionIdx,1);
+                    else message.reactions[existingReactionIdx].emoji=emoji;
+                }
+
+                //We add a new reaction
+                else message.reactions.push({user:reactionUserId,emoji});
+
+                await message.save();
+
+                const populatedMessage=await Message.findById(message?._id)
+                .populate("sender","username profilePicture")
+                .populate("receiver","username profilePicture")
+                .populate("reactions.user","username")
+
+                const updatedReactions={
+                    messageId,
+                    reactions:populatedMessage.reactions
+                }
+
+                const senderSocket=onlineUsers.get(populatedMessage.sender?._id.toString());
+                const receiverSocket=onlineUsers.get(populatedMessage.reciever?._id.toString());
+
+                if(senderSocket) io.to(senderSocket).emit("reaction_update",reactionUpdated);
+                if(receiverSocket) io.to(receiverSocket).emit("reaction_update",reactionUpdate);
+            }
+            catch(error){
+                console.error("Error handling reactions",error);
+            }
+        })
+
+        const handleDisconnected=async()=>{
+            if(!userId) return;
+
+            try{
+                onlineUsers.delete(userId);
+
+                //Clear all typing timeouts
+                if(typingUsers.has(userId)){
+                    const userTyping=typingUsers.get(userId);
+                    Object.keys(userTyping).forEach((key)=>{
+                        if(key.endsWith("_timeout")) clearTimeout(userTyping[key]);
+                    })
+
+                    typingUsers.delete(userId);
+                }
+
+                await User.findByIdAndUpdate(userId,{
+                    isOnline:false,
+                    lastSeen:new Date()
+                })
+
+                io.emit("user_status",{
+                    userId,
+                    isOnline:false,
+                    lastSeen=new Date()
+                })
+
+                socket.leave(userId);
+            }
+            catch(error){
+                console.error("Error handling disconnection",error);
+            }
+        }
+
+        socket.on("disconnect",handleDisconnected);
+    });
+
+    //Attach the online users map to socket server for external  use
+    io.socketUserMap=onlineUsers;
+
+    return io;
+}
+
+
+module.exports={
+    initializeSocket    
 }
