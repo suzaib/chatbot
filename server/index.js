1025628445905c377da6abd24202ffabd05f29e5
Suzaib <suzaib.work@gmail.com>
2026-07-16 05:06:10 +0530

    new

--- server/index.js
+++ server/index.js
@@ -4,8 +4,13 @@ const cors=require('cors');
 const dotenv=require('dotenv');
 const connectDB=require('./config/dbConnect')
 const bodyParser=require('body-parser');
+
+//Importing Routes
 const authRoute=require('./routes/authRoute');
 const chatRoute=require('./routes/chatRoute');
+const statusRoute=require("./routes/statusRoute");
+
+
 const http=require("http");
 const {initializeSocket}=require("./services/socketService");
 
@@ -29,18 +34,28 @@ app.use(express.json()); //Parse body data
 app.use(cookieParser()); //Parse token on every request
 app.use(bodyParse.urlencoded({extension:true}));
 
+//Database Connection
+connectDB();
 
 //Creating Server
 const server=http.createServer(app);
 const io=initializeSocket(server);
 
-//Database Connection
-connectDB();
+//Apply socket middleware before routes
+app.use((req,res,next)=>{
+    req.io=io;
+    req.socketUserMap=io.socketUserMap;
+    next();
+})
+
 
 
 //Routes
 app.use('/api/auth',authRoute);
 app.user('/api/chat',chatRoute);
+app.use('/api/status',statusRoute);
+
+
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
 })
