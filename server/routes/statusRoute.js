ceee65b5c04b3ac6d969225682baefc4639999c5
Suzaib <suzaib.work@gmail.com>
2026-07-15 19:28:08 +0530

    new

--- server/routes/statusRoute.js
+++ server/routes/statusRoute.js
@@ -1,12 +1,12 @@
 const express=require('express');
 const authMiddleware = require('../middlewares/authMiddleware');
-const chatController=require('../controllers');
+const statusController=require('../controllers/statusController');
 const multerMiddleware=require('../config/cloudinaryConfig');
 
 const router=express.Router();
 
 
-router.post('/send-message',authMiddleware,multerMiddleware,chatController.sendMessage);
+router.post('/',authMiddleware,multerMiddleware,statusController.createStatus);
 router.get('/conversations',authMiddleware,chatController.getConversations);
 router.get('/conversation/:conversationId/messages',authMiddleware,chatController.getMessages);
 router.post('/messages/read',authMiddleware,chatController.markAsRead);
