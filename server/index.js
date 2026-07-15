ea46ca4f81dee4df9a3b42e58889f1422703e43a
Suzaib <suzaib.work@gmail.com>
2026-07-15 18:55:44 +0530

    new

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const connectDB=require('./config/dbConnect')
 const bodyParser=require('body-parser');
 const authRoute=require('./routes/authRoute');
 const chatRoute=require('./routes/chatRoute');
+const http=require("http");
+const {initializeSocket}=require("./services/socketService");
 
 dotenv.config();
 
@@ -13,10 +15,24 @@ const PORT=process.env.PORT;
 
 const app=express();
 
+
+//Setting up Cross origin resource sharing
+const corsOption={
+    origin:process.env.FRONTEND_URL,
+    credentials:true
+}
+
+app.use(cors(corsOption));
+
 //Middlewares
 app.use(express.json()); //Parse body data
 app.use(cookieParser()); //Parse token on every request
 app.use(bodyParse.urlencoded({extension:true}));
+
+
+//Creating Server
+const server=http.createServer(app);
+const io=initializeSocket(server);
 
 //Database Connection
 connectDB();
