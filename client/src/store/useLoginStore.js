6c963af58582047501bf34616955aa0bae57b604
Suzaib <suzaib.work@gmail.com>
2026-07-24 15:17:30 +0530

    NEW

---
+++ client/src/store/useLoginStore.js
@@ -0,0 +1,24 @@
+import {create} from 'zustand';
+import {persist} from 'zustand/middleware';
+
+const useLoginStore=create(
+    persist(
+        (set)=>({
+            step:1,
+            userPhoneData:null,
+            setStep:(step)=>set({step}),
+            setUserPhoneData:(data)=>set({userPhoneData:data}),
+            resetLoginState:()=>set({step:1, userPhoneData:null})
+        }),
+        {
+            name:"login-storage",
+            partialize:(state)=>({
+                step:state.step,
+                userPhoneData:state.userPhoneData,
+            })
+        }
+    )
+)
+
+
+export default useLoginStore;
