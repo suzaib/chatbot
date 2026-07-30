416a98c673a5dd78d1b332ec1a32be4598033672
Suzaib <suzaib.work@gmail.com>
2026-07-31 00:08:37 +0530

    New

--- client/src/pages/UserLogin/Login.tsx
+++ client/src/pages/UserLogin/Login.tsx
@@ -1,8 +1,17 @@
 import { useState } from 'react';
 import useLoginStore from '../../store/useLoginStore';
-import yup from 'yup';
+
+//Import everything from yup and put it inside an object named yup
+import * as yup from 'yup';
+
+import {yupResolver} from "@hookform/resolvers/yup";
+
 //Validation Schema
 const loginValidationSchema=yup
+.object()
+.shape({
+  email:yup.string().nullable().notRequired().matches()
+})
 const Login = () => {
   const {step,setStep,userEmailData,setUserEmailData,resetLoginState}=useLoginStore();
   const [email,setEmail]=useState("");
