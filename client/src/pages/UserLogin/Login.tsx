3cf5127441b516dab6cf4378455edf82c35c0e8e
Suzaib <suzaib.work@gmail.com>
2026-07-30 23:33:03 +0530

    New

--- client/src/pages/UserLogin/Login.tsx
+++ client/src/pages/UserLogin/Login.tsx
@@ -1,5 +1,13 @@
-import useLogin
+import { useState } from 'react';
+import useLoginStore from '../../store/useLoginStore';
+import yup from 'yup';
+//Validation Schema
+const loginValidationSchema=yup
 const Login = () => {
+  const {step,setStep,userEmailData,setUserEmailData,resetLoginState}=useLoginStore();
+  const [email,setEmail]=useState("");
+  const [otp,setOTP]=useState(["","","","","",""]);
+  
   return (
     <div>Login</div>
   )
