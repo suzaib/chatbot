412d55ae79343916f582b5daa86cc07a0e30148e
Suzaib <suzaib.work@gmail.com>
2026-08-10 16:25:01 +0530

    New

--- client/src/pages/UserLogin/Login.tsx
+++ client/src/pages/UserLogin/Login.tsx
@@ -3,7 +3,7 @@ import useLoginStore from '../../store/useLoginStore';
 import useUserStore from "../../store/useUserStore";
 import useThemeStore from "../../store/useThemeStore";
 import { motion } from 'framer-motion';
-import {sendOtp,verifyOtp} from "../../services/user.service.js";
+import { sendOtp, verifyOtp } from "../../services/user.service.js";
 
 
 
@@ -11,7 +11,7 @@ import {sendOtp,verifyOtp} from "../../services/user.service.js";
 import * as yup from 'yup';
 
 import { yupResolver } from "@hookform/resolvers/yup";
-import { FaWhatsapp } from 'react-icons/fa';
+import { FaArrowLeft, FaPlus, FaUser, FaWhatsapp } from 'react-icons/fa';
 import Spinner from '../../utils/Spinner';
 
 //Validation Schema
@@ -48,7 +48,7 @@ const avatars = [
 const Login = () => {
   const { step, setStep, userEmailData, setUserEmailData, resetLoginState } = useLoginStore();
   const [email, setEmail] = useState("");
-  const [otp, setOTP] = useState(["", "", "", "", "", ""]);
+  const [otp, setOtp] = useState(["", "", "", "", "", ""]);
   const [selectedAvatar, setSelectedAvatar] = useState(avatars[0]);
 
   const [profilePictureFile, setProfilePictureFile] = useState(null); //This stores the actual file
@@ -61,7 +61,7 @@ const Login = () => {
 
   const { setUser } = useUserStore();
 
-  const [loading,setLoading]=useState(false);
+  const [loading, setLoading] = useState(false);
 
   const {
     register: loginRegister,
@@ -82,39 +82,39 @@ const Login = () => {
     watch
   } = useForm({ resolver: yupResolver(profileValidationSchema) });
 
-  const onLoginSubmit=async()=>{
-    try{
+  const onLoginSubmit = async () => {
+    try {
       setLoading(true);
-      if(email){
-        const response=await sendOtp(email);
-        if(response.status==='success'){
+      if (email) {
+        const response = await sendOtp(email);
+        if (response.status === 'success') {
           toast.info("OTP is sent to your email");
-          setUserEmailData({email});
+          setUserEmailData({ email });
           setStep(2);
         }
       }
     }
-    catch(error){
+    catch (error) {
       console.error(error);
       setError(error.message || "Failed to send OTP");
     }
-    finally{
+    finally {
       setLoading(false);
     }
   }
 
-  const onOtpSubmit=async()=>{
-    try{
+  const onOtpSubmit = async () => {
+    try {
       setLoading(true);
-      if(!userEmailData) throw new Error("Email data is missing");
+      if (!userEmailData) throw new Error("Email data is missing");
 
-      const otpString=otp.join("");
+      const otpString = otp.join("");
       let response;
-      if(userEmailData?.email) response=await verifyOtp(email,otpString);
-      if(response.status==='success'){
+      if (userEmailData?.email) response = await verifyOtp(email, otpString);
+      if (response.status === 'success') {
         toast.success("Otp is verified successfully");
-        const user=response.data?.user;
-        if(user?.username && user?.profilePicture){
+        const user = response.data?.user;
+        if (user?.username && user?.profilePicture) {
           setUser(user);
           toast.success("Welcome to sphinx");
           navigate('/');
@@ -123,58 +123,58 @@ const Login = () => {
         else setStep(3);
       }
     }
-    catch(error){
+    catch (error) {
       console.error(error);
       setError(error.message || "Failed to verify OTP");
     }
-    finally{
+    finally {
       setLoading(false);
     }
   }
 
-  const handleChange=(e)=>{
-    const file=e.target.files[0];
-    if(file){
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
       setProfilePictureFile(file);
       setProfilePicture(URL.createObjectURL(file));
     }
   }
 
-  const onProfileSubmit=async(data)=>{
-    try{
+  const onProfileSubmit = async (data) => {
+    try {
       setLoading(true);
-      const formData=new FormData();
-      formData.append("username",data.username);
-      formData.append("agreed",data.agreed);
-      if(profilePictureFile) formData.append("media",profilePictureFile);
-      else formData.append("profilePicture",selectedAvatar);
+      const formData = new FormData();
+      formData.append("username", data.username);
+      formData.append("agreed", data.agreed);
+      if (profilePictureFile) formData.append("media", profilePictureFile);
+      else formData.append("profilePicture", selectedAvatar);
 
       await updatedUserProfile(formData);
       toast.success("Welcome back to sphinx");
       navigate('/');
       resetLoginState();
     }
-    catch(error){
+    catch (error) {
       console.error(error);
       setError(error.message || "Failed to update user profile");
     }
-    finally{
+    finally {
       setLoading(false);
     }
   }
 
-  const handleOtpChange=(idx,val)=>{
-    const newOtp=[...otp];
-    newOtp[idx]=val;
-    setOtp(netOtp);
-    setOtpValue("otp",newOtp.join(""));
-    if(val && idx<5) document.getElementById(`otp-${idx+1}`)?.focus();
+  const handleOtpChange = (idx, val) => {
+    const newOtp = [...otp]; //Creating a copy of the old otp array
+    newOtp[idx] = val;
+    setOtp(newOtp);
+    setOtpValue("otp", newOtp.join(""));
+    if (val && idx < 5) document.getElementById(`otp-${idx + 1}`)?.focus();
   }
 
-  const handleBack=()=>{
+  const handleBack = () => {
     setStep(1);
     setUserEmailData(null);
-    setOtp(["","","","","",""]);
+    setOtp(["", "", "", "", "", ""]);
     setError("");
   }
 
@@ -230,7 +230,7 @@ const Login = () => {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 placeholder="Email"
-                className={`w-2/3 px-4 py-2 border ${theme === 'dark' ? "bg-gray-700 border-gray-600 text-white" : "bg-white border-gray-300"} rounded-md focus:outline-none focus:right-2 focus:ring-green-500 ${loginErrors.email ? "border-red-500":""}`}/>
+                className={`w-2/3 px-4 py-2 border ${theme === 'dark' ? "bg-gray-700 border-gray-600 text-white" : "bg-white border-gray-300"} rounded-md focus:outline-none focus:right-2 focus:ring-green-500 ${loginErrors.email ? "border-red-500" : ""}`} />
             </div>
             {loginErrors.email && (
               <p className="text-red-500 text-sm">
@@ -242,8 +242,111 @@ const Login = () => {
           <button
             type="submit"
             className="w-full bg-green-500 text-white py-2 rounded-md hover:bg-green-600 transition">
-            {loading? <Spinner/> : "Send OTP"}
+            {loading ? <Spinner /> : "Send OTP"}
           </button>
+        </form>
+      )}
+
+      {step === 2 && (
+        <form onSubmit={handleOtpSubmit(onOtpSubmit)} className="space-y-4">
+          <p className={`text-center ${theme === 'dark' ? "text-gray-300" : "text-gray-600"} mb-4`}>
+            Please enter the 6 digit otp sent to {userEmailData.email}
+          </p>
+          <div className='flex justify-between'>
+            {otp.map((dig, idx) => (
+              <input
+                key={idx}
+                id={`otp-${idx}`}
+                type="text"
+                maxLength={1}
+                value={dig}
+                onChange={(e) => handleOtpChange(idx, e.target.value)}
+                className={`w-12 h-12 text-center border ${theme === 'dark' ? "bg-gray-700 border-gray-600 text-white" : "bg-white border-gray-300"} rounded-md focus:outline-none focus:ring-green-500 ${otpErrors.otp ? "border-red-500" : ""}`}
+              />
+            ))}
+          </div>
+          {otpErrors.otp && (
+            <p className="text-red-500 text-sm">
+              {otpErrors.otp.message}
+            </p>
+          )}
+
+          <button type="submit" className="w-full bg-green-500 text-white py-2 rounded-md hover:bg-green-600 transition">
+            {loading ? <Spinner /> : "Verify OTP"}
+          </button>
+
+          <button type="button" onClick={handleBack} className={`w-full mt-2 ${theme === 'dark' ? "bg-gray-700 text-gray-300" : "bg-gray-200 text-gray-700"} py-2 rounded-md hover:bg-gray-300 transition flex items-center justify-center`}>
+            <FaArrowLeft className="mr-2" />
+            Wrong Email? Go Back
+          </button>
+
+        </form>
+      )}
+
+
+      {step === 3 && (
+        <form onSubmit={handleProfileSubmit(onProfileSubmit)} className='space-y-4'>
+          <div className='flex flex-col items-center mb-4'>
+            <div className='relative w-24 h-24 mb-2'>
+              <img src={profilePicture || selectedAvatar} alt="profile" className='w-full h-full rounded-full object-cover' />
+              <label htmlFor='profile-picture' className='absolute bottom-0 right-0 bg-green-500 text-white p-2 rounded-full cursor-pointer hover:bg-green-600 transition duration-300'>
+                <FaPlus className="w-4 h-4" />
+              </label>
+              <input type="file" id="profile-picture" accept='image/*' onChange={handleFileChange} className='hidden' />
+            </div>
+
+            <p className={`text-sm ${theme === 'dark' ? "text-gray-300" : "text-gray-500"} mb-2`}>
+              Choose an avatar
+            </p>
+
+            <div className='flex flex-wrap justify-center gap-2'>
+              {avatars.map((avatar, idx) => (
+                <img key={idx} src={avatar} alt={`Avatar-${idx + 1}`} onClick={() => setSelectedAvatar(avatar)} className={`w-12 h-12 rounded-full cursor-pointer transition duration-300 ease-in-out transform hover:scale-110 ${selectedAvatar === avatar ? "ring-2 ring-green-500" : ""}`} />
+              ))}
+            </div>
+          </div>
+
+          <div className='relative'>
+            <FaUser className={`absolute left-3 top-1/2 transform -translate-y-1/2 ${theme === 'dark' ? "text-gray-400" : "text-gray-600"}`} />
+            <input
+              {...profileRegister("username")}
+              type="text"
+              placeholder="Username"
+              className={`w-full pl-10 pr-3 py-2 border {theme==='dark'? "bg-gray-700 border-gray-600 text-white":"bg-white border-gray-300"} rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 text-lg`}
+            />
+
+            {profileErrors.username && (
+              <p className='text-red-500 text-sm mt-1'>
+                {profileErrors.username.message}
+              </p>
+            )}
+          </div>
+
+          <div className='flex items-center space-x-2'>
+            <input
+              {...profileRegister('agreed')}
+              type="checkbox"
+              className={`rounded ${theme === 'dark' ? "text-green-500 bg-gray-700" : "text-green-500"} focus:ring-green-500`}
+            />
+            <label htmlFor='terms' className={`text-sm ${theme === 'dark' ? "text-gray-300" : "text-gray-700"}`}>
+              I agree to the {""}
+              <a href="#" className='text-red-500 text-sm mt-1'>
+                Terms and Conditions
+              </a>
+            </label>
+
+          </div>
+          
+          {profileErrors.agreed && (
+            <p className='text-red-500 hover:underline'>
+              {profileErrors.agreed.message}
+            </p>
+          )}
+
+          <button type="submit" disabled={!watch("agreed") || loading} className={`w-full bg-green-500 text-white font-bold py-3 px-4 rounded-md transition duration-300 ease-in-out transform hover:scale-105 flex items-center justify-center text-lg ${loading ? "opacity-50 cursor-not-allowed" : ""}`}>
+            {loading ? <Spinner /> : "Create Profile"}
+          </button>
+
         </form>
       )}
 
