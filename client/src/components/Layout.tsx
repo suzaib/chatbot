7e6df615fd40a91585da72b4452ce4b3af3964e5
Suzaib <suzaib.work@gmail.com>
2026-08-14 02:17:19 +0530

    NEW

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,34 +1,36 @@
-import { motion,AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import ChatWindow from "../pages/ChatSection/ChatWindow";
+import Sidebar from "./Sidebar";
 
-const Layout = ({children,isThemeDialogueOpen,toggleThemeDialogue,isStatusPreviewOpen,statusPreviewContent}) => {
+const Layout = ({ children, isThemeDialogueOpen, toggleThemeDialogue, isStatusPreviewOpen, statusPreviewContent }) => {
 
-  const selectedContact=useLayoutStore(state=>state.selectedContact);
-  const setSelectedContact=useLayoutStore(state=>state.setSelectedContact);
-  const location=useLocation();
-  const [isMobile,setIsMobile]=useState(window.innerWidth<760);
-  const {theme,setTheme}=useThemeStore();
+  const selectedContact = useLayoutStore(state => state.selectedContact);
+  const setSelectedContact = useLayoutStore(state => state.setSelectedContact);
+  const location = useLocation();
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 760);
+  const { theme, setTheme } = useThemeStore();
 
-  useEffect(()=>{
-    const handleResize=()=>{
-      setIsMobile(window.innerWidth<768)
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768)
     }
 
-    window.addEventListener("resize",handleResize);
-    return ()=> window.removeEventListener("resize",handleResize);
-  },[])
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [])
   return (
-    <div className={`min-h-screen ${theme==='dark'? "bg-[#111b21] text-white":"bg-gray-100 text-black"} flex-relative`}>
-      {!isMobile && <Sidebar/>}
-      <div className={`flex-1 flex overflow-hidden ${isMobile? "flex-col":""}`}>
+    <div className={`min-h-screen ${theme === 'dark' ? "bg-[#111b21] text-white" : "bg-gray-100 text-black"} flex-relative`}>
+      {!isMobile && <Sidebar />}
+      <div className={`flex-1 flex overflow-hidden ${isMobile ? "flex-col" : ""}`}>
         <AnimatePresence initial={false}>
           {(!selectedContact || !isMobile) && (
-            <motion.div 
-              key="chatlist" 
-              initial={{x:isMobile? "-100%":0}} 
-              animate={{x:0}}
-              exit={{x:"100%"}}
-              transition={{type:"tween"}}
-              className={`w-full md:w-2/5 h-full ${isMobile? "pb-16":""}`}
+            <motion.div
+              key="chatlist"
+              initial={{ x: isMobile ? "-100%" : 0 }}
+              animate={{ x: 0 }}
+              exit={{ x: "100%" }}
+              transition={{ type: "tween" }}
+              className={`w-full md:w-2/5 h-full ${isMobile ? "pb-16" : ""}`}
             >
               {children}
             </motion.div>
@@ -36,17 +38,65 @@ const Layout = ({children,isThemeDialogueOpen,toggleThemeDialogue,isStatusPrevie
           {(selectedContact || !isMobile) && (
             <motion.div
               key="chatWindow"
-              initial={{x:isMobile? "-100%":0}}
-              animate={{x:0}}
-              exit={{x:"-100%"}}
-              transition={{type:"tween"}}
+              initial={{ x: isMobile ? "-100%" : 0 }}
+              animate={{ x: 0 }}
+              exit={{ x: "-100%" }}
+              transition={{ type: "tween" }}
               className="w-full h-full"
             >
-              {children}
+              <ChatWindow
+                selectedContact={selectedContact}
+                setSelectedContact={setSelectedContact}
+                isMobile={isMobile} />
             </motion.div>
           )}
         </AnimatePresence>
       </div>
+
+      {isMobile && <Sidebar />}
+
+      {isThemeDialogueOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div className={`${theme === 'dark' ? "bg-[#202c33] text-white" : "bg-white text-black"} p-6 rounded-lg shadow-lg max-w-sm w-full`}>
+            <h2 className="text-2xl font-semibold mb-4">
+              Choose a theme
+            </h2>
+            <div className="space-y-4">
+              <label className="flex items-center space-x-3 cursor-pointer">
+                <input
+                  type="radio"
+                  value="light"
+                  checked={theme === 'light'}
+                  onChange={() => setTheme("light")}
+                  className="from-radio text-blue-600" />
+                <span>Light</span>
+              </label>
+
+              <label className="flex items-center space-x-3 cursor-pointer">
+                <input
+                  type="radio"
+                  value="dark"
+                  checked={theme === 'dark'}
+                  onChange={() => setTheme("dark")}
+                  className="from-radio text-blue-600" />
+                <span>Dark</span>
+              </label>
+            </div>
+            <button
+              onClick={toggleThemeDialogue}
+              className="mt-6 w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-600 transition duration-200"
+            >
+              Close
+            </button>
+
+          </div>
+        </div>
+      )}
+
+      {/* status preview */}
+      {isStatusPreviewOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"> {statusPreviewContent}</div>
+      )}
     </div>
   )
 }
