d8c24142df6b38a55018b8dcda2af9fc73b15731
Suzaib <suzaib.work@gmail.com>
2026-08-14 05:27:48 +0530

    NEW

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,7 +1,73 @@
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import useUserStore from "../store/useUserStore";
+import useThemeStore from "../store/themeStore";
+import useLayoutStore from "../store/useLayoutStore";
+import { FaWhatsapp } from "react-icons/fa";
+import {motion} from 'framer-motion';
 
 const Sidebar = () => {
+
+  const location = useLocation();
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 760);
+  const { theme, setTheme } = useThemeStore();
+  const {user}=useUserStore();
+  const {activeTab,setActiveTab,selectedContact}=useLayoutStore();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768)
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [])
+
+  useEffect(()=>{
+    if(location.pathname==='/') setActiveTab('chats');
+    else if(location.pathname=='/status') setActiveTab('status');
+    else if(location.pathname==='/user-profile') setActiveTab('profile');
+    else if(location.pathname==='/settings') setActiveTab('settings');
+  },[location,setActiveTab])
+
+  //When we open a chat on mobile, the chat covers the screen completely, therefore no sidebar
+  if(isMobile && selectedContact) return null; 
+
+  const SidebarContent=(
+    <>
+      <Link 
+        to="/"
+        className={`${isMobile? "":"mb-8"} ${activeTab==='chats' && 'bg-gray-300 shadow-sm p-2 rounded-full'} focus:outline-none`}>
+        <FaWhatsapp className={`h-6 w-6 ${activeTab==='chats'? theme==='dark'?"text-gray-800":"":theme==='dark'? "text-gray-300":"text-gray-800"}`}/>
+      </Link>
+
+      <Link 
+        to="/status"
+        className={`${isMobile? "":"mb-8"} ${activeTab==='chats' && 'bg-gray-300 shadow-sm p-2 rounded-full'} focus:outline-none`}>
+        <FaWhatsapp className={`h-6 w-6 ${activeTab==='chats'? theme==='dark'?"text-gray-800":"":theme==='dark'? "text-gray-300":"text-gray-800"}`}/>
+      </Link>
+
+      <Link 
+        to="/"
+        className={`${isMobile? "":"mb-8"} ${activeTab==='chats' && 'bg-gray-300 shadow-sm p-2 rounded-full'} focus:outline-none`}>
+        <FaWhatsapp className={`h-6 w-6 ${activeTab==='chats'? theme==='dark'?"text-gray-800":"":theme==='dark'? "text-gray-300":"text-gray-800"}`}/>
+      </Link>
+
+      <Link 
+        to="/"
+        className={`${isMobile? "":"mb-8"} ${activeTab==='chats' && 'bg-gray-300 shadow-sm p-2 rounded-full'} focus:outline-none`}>
+        <FaWhatsapp className={`h-6 w-6 ${activeTab==='chats'? theme==='dark'?"text-gray-800":"":theme==='dark'? "text-gray-300":"text-gray-800"}`}/>
+      </Link>
+    </>
+  )
   return (
-    <div>Sidebar</div>
+    <motion.div 
+      initial={{opacity:0}}
+      animate={{opacity:1}}
+      transition={{duration:0.3}}
+      className={`${isMobile? "fixed bottom-0 left-0 right-0 h-16":"w-16 h-screen border-r-2"} ${theme==='dark'? "bg-gray-800 border-gray-600":"bg-[rgb(239,242,254)] border-gray-300"} ${isMobile? "flex-row justify-around":"flex-col justify-between"} bg-opacity-90 flex items-center py-4 shadow-lg`}>
+      {SidebarContent}
+    </motion.div>
   )
 }
 
