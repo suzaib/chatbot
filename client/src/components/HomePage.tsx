5e72cdffe800474ca8515243c4a11ced97795302
Suzaib <suzaib.work@gmail.com>
2026-08-14 04:11:46 +0530

    NEW

--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -1,7 +1,37 @@
+import ChatList from "../pages/ChatSection/ChatList"
+import Layout from "./Layout"
+import {motion} from 'framer-motion';
 
 const HomePage = () => {
+
+  const setSelectedContact=useLayoutStore(
+    (state)=>state.setSelectedContact
+  );
+
+  const [allUsers,setAllUsers]=useState([]);
+  const getUser=async()=>{
+    try{
+      const result=await getAllUsers();
+      if(result.status==='success') setAllUsers(result.data);
+    }
+    catch(error){
+      console.error(error);
+    }
+  }
+
+  useEffect(()=>{
+    getAllUsers();
+  },[])
   return (
-    <div>HomePage</div>
+    <Layout>
+      <motion.div
+        initial={{opacity:0}}
+        animate={{opacity:1}}
+        transition={{duration:0.5}}
+        className="h-full">
+        <ChatList contacts={allUsers} setSelectedContact={setSelectedContact}/>
+      </motion.div>
+    </Layout>
   )
 }
 
