0b6128d37060039b5e0090874c7507c2c185c6cd
Suzaib <suzaib.work@gmail.com>
2026-08-17 22:44:17 +0530

    New

--- client/src/pages/ChatSection/ChatList.tsx
+++ client/src/pages/ChatSection/ChatList.tsx
@@ -1,7 +1,54 @@
+import { useState } from "react";
+import { FaPlus, FaSearch } from "react-icons/fa";
+import {motion} from "framer-motion";
 
-const ChatList = () => {
+const ChatList = ({contacts}) => {
+  const setSelectedContact=useLayoutStore(
+    (state)=>state.setSelectedContact
+  );
+
+  const selectedContact=useLayoutStore((state)=> state.selectedContact);
+  const {theme}=useThemeStore();
+  const {user}=useUserStore();
+  const [searchTerms,setSearchTerms]=useState("");
+
+  const filteredContacts=contacts?.filter((contact)=>
+    contact?.username?.toLowerCase().includes(searchTerms.toLowerCase()));
   return (
-    <div>ChatList</div>
+    <div className={`w-full border-r h-screen ${theme==='dark'? "bg-[rgb(17,27,33)] border-gray-600":"bg-white border-gray-200"}`}>
+      <div className={`p-4 flex justify-between ${theme==='dark'? "text-white":"text-gray-800"}`}>
+        <h2 className="text-xl font-semibold">
+          Chats
+        </h2>
+        <button className="p-2 bg-green-500 text-white rounded-full hover:bg-green-600 transition-colors">
+          <FaPlus/>
+        </button>
+      </div>
+      <div className="p-2">
+        <div className="relative">
+          <FaSearch className={`absolute left-3 top-1/2 transform -translate-y-1/2 ${theme==='dark'? "text-gray-400":"text-gray-800"}`}/>
+          <input 
+            type="text"
+            placeholder="Search or start a new chat"
+            className={`w-full pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 ${theme==='dark'? "bg-gray-800 text-white border-gray-700 placeholder-gray-500":"bg-gray-100 text-black border-gray-200 placeholder-gray-400"}`}
+            value={searchTerms}
+            onChange={(e)=>setSearchTerms(e.target.value)}
+          />
+
+        </div>
+      </div>
+      <div className="overflow-y-auto h-[calculate(100vh-120px)]">
+        {filteredContacts.map((contact)=>(
+          <motion.div
+            key={contact.id}
+            onClick={()=>setSelectedContact(contact)}
+            className={`p-3 flex items-center cursor-pointer ${theme==='dark'? selectedContact?._id===contact._id ? "bg-gray-700":"hover:bg-gray-800": selectedContact._id===contact._id? "bg-gray-200":"hover:bg-gray-100"}`}
+            >
+
+          </motion.div>
+        ))}
+      </div>
+    </div>
   )
 }
 
