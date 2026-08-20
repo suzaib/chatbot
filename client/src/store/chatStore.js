4b1c7948827e83bccdc922aab954a5f60f95318e
Suzaib <suzaib.work@gmail.com>
2026-08-20 09:35:24 +0530

    New

--- client/src/store/chatStore.js
+++ client/src/store/chatStore.js
@@ -42,12 +42,63 @@ const useChatStore=create((set,get)=>({
             }))
         })
 
-        //Handle Reaction
+        //Handle reaction on message
         socket.on("reaction_update",({messageId,reactions})=>{
             set((state)=>({
                 messages:state.messages.map((msg)=>
                 msg._id===messageId ? {...msg,reactions}:msg)
             }))
         })
+
+        //Handle message removal from local state
+        socket.on("message_deleted",({deletedMessageId})=>{
+            set((state)=>({
+                message:state.messages.filter((msg)=>msg._id!=deletedMessageId)
+            }))
+        })
+
+        //Handle any message sending error
+        socket.on("message_error",(error)=>{
+            console.error("message error",error)
+        })
+
+        //Listener for typing users
+        //The map stores conversationIDs->Set of userIDs
+        //So the map looks like this :
+        // ConvoID1->{user1, user2}
+        socket.on("user_typing",({userId,conversationId,isTyping})=>{
+            set((state)=>{
+                const newTypingUsers=new Map(state.typingUsers);
+
+                //First we check whether the convoId is there in the typing users map
+                //If not then we add it there
+                if(!newTypingUsers.has(conversationId)) newTypingUsers.set(conversationId,new Set());
+
+                //Then we get the set from the conversationID
+                const typingSet=newTypingUsers.get(conversationId);
+
+                //If the user is typing, we add him to the set, otherwise we remove him
+                //Since we are using a set, we can't add it twice
+                if(isTyping) typingSet.add(userId);
+                else typingSet.delete(userId);
+
+                return {typingUsers:newTypingUsers};
+            })
+        })
+
+        //Track user's online/offline status
+        socket.on("user_status",({userId,isOnline,lastSeen})=>{
+            set((state)=>{
+                const newOnlineUsers=new Map(state.onlineUsers);
+                newOnlineUsers.set(userId,{isOnline,lastSeen});
+                return {isOnline:newOnlineUsers}
+            })
+        })
+
+        //Emit status check for all users in conversation list
+        const {conversations}=get();
+        if()
+
+
     }
 }))
