86ef56d70fbb41ffa312d2a09d1ef41aab92617d
Suzaib <suzaib.work@gmail.com>
2026-08-22 22:30:45 +0530

    New

--- server/delete.js
+++
@@ -1,7 +0,0 @@
-socket.on("user_status",({userId,isOnline,lastSeen})=>{
-    set((state)=>{
-        const newOnlineUsers=new Map(state.onlineUsers);
-        newOnlineUsers.set(userId,{isOnline,lastSeen});
-        return {isOnline:newOnlineUsers}
-    })
-})
