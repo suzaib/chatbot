86470bb8865dec41eef0489ab969e68394453469
Suzaib <suzaib.work@gmail.com>
2026-08-26 05:26:47 +0530

    New

--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -1,41 +1,41 @@
 import { useEffect, useState } from "react";
-import ChatList from "../pages/ChatSection/ChatList"
-import Layout from "./Layout"
-import {motion} from 'framer-motion';
+import ChatList from "../pages/ChatSection/ChatList";
+import Layout from "./Layout";
+import { motion } from 'framer-motion';
 import type { User } from "../types/user";
 import useLayoutStore from "../store/useLayoutStore";
-import {getAllUsers} from "../services/user.service";
+import { getAllUsers } from "../services/user.service";
 
 const HomePage = () => {
-  const [allUsers,setAllUsers]=useState<User[]>([]);
-  const setSelectedContact=useLayoutStore(
-    (state)=>state.setSelectedContact
+  const [allUsers, setAllUsers] = useState<User[]>([]);
+  const setSelectedContact = useLayoutStore(
+    (state) => state.setSelectedContact
   )
 
   //Async functions always return a promise
   //Function to get all the users
-  const getAllUser=async():Promise<void> =>{
-    try{
-      const result=await getAllUsers();
-      if(result.status==='success') setAllUsers(result.data);
+  const fetchAllUsers = async (): Promise<void> => {
+    try {
+      const result = await getAllUsers();
+      if (result.status === 'success') setAllUsers(result.data);
     }
-    catch(error){
+    catch (error) {
       console.error(error);
     }
   }
 
-  useEffect(()=>{
-    getAllUser();
-  },[])
+  useEffect(() => {
+    fetchAllUsers();
+  }, [])
   return (
     <Layout>
       <motion.div
-        initial={{opacity:0}}
-        animate={{opacity:1}}
-        transition={{duration:0.5}}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
         className="h-full">
-        <ChatList 
-          contacts={allUsers} 
+        <ChatList
+          contacts={allUsers}
           setSelectedContact={setSelectedContact}
         />
       </motion.div>
