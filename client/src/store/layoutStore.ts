2115e418be6c41088e291e728ae4318dd9bc7077
Suzaib <suzaib.work@gmail.com>
2026-08-26 21:54:31 +0530

    New

--- client/src/store/layoutStore.ts
+++ client/src/store/layoutStore.ts
@@ -1,17 +1,27 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+import { persist, createJSONStorage } from 'zustand/middleware';
+import type { User } from '../types/user';
 
-const useLayoutStore = create(
+type ActiveTab = 'chats' | 'profile' | 'settings';
+
+interface LayoutState {
+    activeTab: ActiveTab;
+    selectedContact: User | null;
+    setSelectedContact: (contact: User | null) => void; //void here means the function doesn't return a value
+    setActiveTab: (tab: ActiveTab) => void;
+}
+
+const useLayoutStore = create<LayoutState>()(
     persist(
         (set) => ({
             activeTab: 'chats',
             selectedContact: null,
-            setSelectedContact: (contact) => set({ setSelectedContact }),
-            setActiveTab: (tab) => set({ activeTab })
+            setSelectedContact: (contact) => set({ selectedContact: contact }),
+            setActiveTab: (tab) => set({ activeTab: tab })
         }),
         {
             name: "layout-storage",
-            getStorage: () => localStorage,
+            storage: createJSONStorage(() => localStorage),
         }
     )
 )
